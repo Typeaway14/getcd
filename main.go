7a61c4d6f0fda54eab9a34184bd1974d0164616d
Typeaway14 <harsh.som33@gmail.com>
2024-04-18 23:49:07 +0530

    added put and delete; some error but no time woops

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"context" //ask Navin anna
 	"fmt"
-	"go.etcd.io/etcd/client/v3"
 	"log"
+	"os"
 	"time"
+
+	"go.etcd.io/etcd/client/v3"
+	"google.golang.org/genproto/googleapis/api/error_reason"
 )
 
 func main() {
+
+	args := os.Args
+
 	cli, err := clientv3.New(clientv3.Config{
 		Endpoints:   []string{"localhost:2379", "localhost:22379", "localhost:32379"},
 		DialTimeout: 5 * time.Second,
@@ -18,9 +24,38 @@ func main() {
 	}
 	defer cli.Close()
 
-	_, err = cli.Put(context.Background(), "rinku", "tingupingu")
-	if err != nil {
-		log.Fatal(err) //custom message esketit
-	}
+	// _, err = cli.Put(context.Background(), "rinku", "tingupingu")
+	// if err != nil {
+	// 	log.Fatal(err) //custom message esketit
+	// }
 	fmt.Println("Put operation completed successfully!")
+	
+	//TODO: switch case type tings
+
+	err = Put(cli, args)
+	if err != nil {
+		log.Fatal(err) //custom message esketit x2
+	}
 }
+
+func Put(cli *clientv3.Client,args) error {
+	_,err := cli.Put(context.Background(), key, value)
+	if err != nil{
+		return err
+	}
+	fmt.Println("Put Operation completed successfully!")
+	// TODO: log at normal 
+	return nil
+}
+
+func Delete(cli *clientv3.Client, args) error
+{
+	_, err := cli.Delete(context.Background(),key)
+	if err != nil{
+		return err
+	}
+	fmt.Println("Delete operation completed successfully!")
+	// TODO: log at normal
+	return nil
+}
+
